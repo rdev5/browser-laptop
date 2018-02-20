76954af05eaa7b3bdff8866846ff80a9b492eac5
yan <yan@mit.edu>
2018-02-20 15:54:28 -0800

    UI and misc front-end changes for tor
    Disable search suggestions in private mode for Tor
    Fix https://github.com/brave/browser-laptop/issues/13064
    
    set ddg as default search engine in tor private tabs
    fix #13212
    
    Disable webtorrent in tor private mode
    fix #13063
    
    load favicons in Tor tabs as data: URLs
    fix #13065
    
    Omit unlocked icon for HTTP onion sites
    Fix https://github.com/brave/browser-laptop/issues/12990
    also fix unittests
    Test Plan:
    1. go to http://3expgpdnrrzezf7r.onion/ in a private tab
    2. no icon should be in the urlbar
    3. go to https://3g2upl4pq6kufc4m.onion/
    4. you should see a lock icon

--- app/filtering.js
+++ app/filtering.js
@@ -638,7 +638,7 @@ function registerForMagnetHandler (session) {
 }
 
 module.exports.setTorNewIdentity = (url, tabId) => {
-  const ses = session.fromPartition('persist:tor')
+  const ses = session.fromPartition(appConfig.tor.partition)
   if (!ses || !url) {
     return
   }
@@ -655,7 +655,7 @@ function initSession (ses, partition) {
 }
 
 const initPartition = (partition) => {
-  const isTorPartition = partition === 'persist:tor'
+  const isTorPartition = partition === appConfig.tor.partition
   // Partitions can only be initialized once the app is ready
   if (!app.isReady()) {
     partitionsToInitialize.push(partition)
